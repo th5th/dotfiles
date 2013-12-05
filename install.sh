556ab1862c797f6ae6a7acf28ae2b361ba1950fc
Ed Willson <ed.willson24@gmail.com>
2013-12-05 23:14:52 +0000

    Remove files before creating links when installing

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ fi
 
 for f in .[^.]*; do
     if [[ ! -d "$f" ]]; then
+        if [[ -e $INSTALL_DIR/"$f" ]]; then
+            rm -i $INSTALL_DIR/"$f"
+        fi
+
         ln "$f" $INSTALL_DIR/"$f"
     fi
 done
